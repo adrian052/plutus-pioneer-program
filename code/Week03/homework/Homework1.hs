395330290aa9edc30c4f1a761a83c1bb1a242e7e
adrian052 <a_drian1@hotmail.es>
2024-05-08 12:56:28 -0600

    Week03 homework completed

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -7,11 +7,22 @@
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
-                                       mkValidatorScript)
+
+import Plutus.V1.Ledger.Interval (contains,before)
+import Plutus.V2.Ledger.Contexts (txSignedBy)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    POSIXTime,
+    PubKeyHash,
+    ScriptContext (scriptContextTxInfo),
+    TxInfo (txInfoValidRange),
+    Validator,
+    from,
+    to,
+    mkValidatorScript,
+  )
 import           PlutusTx             (compile, unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool (..))
+import           PlutusTx.Prelude     (Bool (..),($), (&&),(||),not,(>))
 import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
@@ -29,7 +40,15 @@ unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkVestingValidator _dat () _ctx = False -- FIX ME!
+mkVestingValidator dat () ctx =
+    (beneficiary1HasSigned && isBeforeDeadline) || (beneficiary2HasSigned && isAfterDeadline)
+  where
+    info = scriptContextTxInfo ctx
+    beneficiary1HasSigned = txSignedBy info (beneficiary1 dat)
+    beneficiary2HasSigned = txSignedBy info (beneficiary2 dat)
+    isBeforeDeadline = contains (to $ deadline dat) $ txInfoValidRange info
+    lowerPosixTime = ()
+    isAfterDeadline = before (deadline dat) (txInfoValidRange info) 
 
 {-# INLINABLE  mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
--- code/Week03/homework/Homework2.hs
+++ code/Week03/homework/Homework2.hs
@@ -6,12 +6,21 @@
 {-# LANGUAGE TemplateHaskell       #-}
 
 module Homework2 where
-
-import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
-                                       mkValidatorScript)
+import Plutus.V1.Ledger.Interval (before, after, contains)
+import Plutus.V2.Ledger.Contexts (txSignedBy)
+import Plutus.V2.Ledger.Api
+  ( BuiltinData,
+    POSIXTime,
+    PubKeyHash,
+    ScriptContext (scriptContextTxInfo),
+    TxInfo (txInfoValidRange),
+    Validator,
+    from,
+    to,
+    mkValidatorScript,
+  )
 import           PlutusTx             (applyCode, compile, liftCode)
-import           PlutusTx.Prelude     (Bool (False), (.))
+import           PlutusTx.Prelude     (Bool (False), (.),(&&),($))
 import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
@@ -20,8 +29,13 @@ import           Utilities            (wrapValidator)
 {-# INLINABLE mkParameterizedVestingValidator #-}
 -- This should validate if the transaction has a signature from the parameterized beneficiary and the deadline has passed.
 mkParameterizedVestingValidator :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkParameterizedVestingValidator _beneficiary _deadline () _ctx = False -- FIX ME!
-
+mkParameterizedVestingValidator beneficiary deadline () ctx = beneficiaryHasSigned && checkDeadline
+        where
+            info = scriptContextTxInfo ctx
+            beneficiaryHasSigned = txSignedBy info (beneficiary)
+            checkDeadline = from deadline `contains` txInfoValidRange info
+            
+            
 {-# INLINABLE  mkWrappedParameterizedVestingValidator #-}
 mkWrappedParameterizedVestingValidator :: PubKeyHash -> BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedParameterizedVestingValidator = wrapValidator . mkParameterizedVestingValidator
